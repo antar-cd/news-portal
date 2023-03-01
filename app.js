715852373afabf923d7cc6bf09ce4e9f2f56514b
antar-cd <sagar.chandradas1994@gmail.com>
2023-03-02 04:21:59 +0600

    single news added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const showCatagories = data =>{
     // capture container catagories to append all the category links
     const  categoriesContainer = document.getElementById('categories-container');
     data.news_category.forEach(singleCategory =>{
-        console.log(singleCategory);
+        //console.log(singleCategory);
 
         // advanced 
        // categoriesContainer.innerHTML += `<a class="nav-link" href="#">${singleCategory.category_name}</a>`
@@ -34,7 +34,63 @@ const fetchCategoryNewse = (category_id,category_name) =>{
 }
 
 const showAllNews =(data,category_name) =>{
-    console.log(data,category_name);
+   // console.log(data,category_name);
     document.getElementById('news-count').innerText =data.length
     document.getElementById('category-name').innerText =category_name
+
+    const newsContainer = document.getElementById('all-news');
+    newsContainer.innerHTML ='';
+    data.forEach(singleNews =>{
+        const {_id,image_url,title,details,author,total_view} =(singleNews);
+        //newsContainer.innerHTML += ``
+        const card =document.createElement('div')
+        card.classList.add('card','mb-3');
+        card.innerHTML = `<div class="row g-0">
+        <div class="col-md-4">
+          <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8 d-flex flex-column">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${details.slice(0,200)}</p>
+          </div>
+          <div class="card-footer border-0 bg-body d-flex justify-content-between">
+                <div class= d-flex gap-3>
+                <img src="${author.img}" class="img-fluid rounded-circle" alt="..." height="40" width="40">
+               <div>
+               <p class="m-0 p-0">${author.name}</p>
+               <p class="m-0 p-0">${author.published_date}</p>
+               </div>
+                </div>
+
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-eye"></i>
+                    <p class="m-0 p-0">${total_view}</p>
+                </div>
+
+                <div>
+                <i class="fas fa-star"></i>
+                </div>
+
+                <div>
+                <i class="fas fa-arrow-right" onclick="fetchNewsDetail('${_id}')"data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                </div>
+          </div>
+          </div>
+        </div>
+      </div>`;
+
+      newsContainer.appendChild(card)
+    });
 }
+    const fetchNewsDetail = news_id =>{
+    let url =`https://openapi.programming-hero.com/api/news/${news_id}`;
+    //console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>showNewsDetail(data.data[0]));
+} 
+    const showNewsDetail = newsDetail =>{
+
+}
+
